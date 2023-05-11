6fd4ffe3980807ca56c1a3262c038c81091dad74
aninarafath6 <arafathanin6@gmail.com>
2023-05-11 22:29:10 +0530

    inital commit

---
+++ app/src/main/java/com/example/arnhss/activites/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.arnhss.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.arnhss.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
